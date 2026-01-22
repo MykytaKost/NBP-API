e434e7411280e825bff6e9d5fbccf28af10f6500
MykytaKost <nikitakostiuchok@gmail.com>
2026-01-22 15:22:26 +0100

    Optimize table rendering. Ensuring accessibility. Added description

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,25 @@
 import "../scss/style.scss";
 
 fetch('https://api.nbp.pl/api/exchangerates/tables/A/?format=json')
-
 .then(response => response.json())
-
 .then(data => {
     const rates = data[0].rates;
     const tbody = document.querySelector('#currencyTable tbody');
+    
+    // Tworzymy fragment dokumentu 
+    const fragment = document.createDocumentFragment();
 
     rates.forEach(rate => {
-
         const row = document.createElement('tr');
-
         row.innerHTML = `
-            <td>${rate.currency}</td>  <!-- nazwa waluty -->
-            <td>${rate.code}</td>      <!-- kod waluty, np. USD -->
-            <td>${rate.mid}</td>       <!-- kurs w PLN -->
+            <td>${rate.currency}</td>
+            <td>${rate.code}</td>
+            <td>${rate.mid}</td>
         `;
-
-        tbody.appendChild(row);
+        fragment.appendChild(row);
     });
-})
 
-.catch(() => alert('Nie udało się pobrać danych z NBP.'));
+    // Wstawiamy wszystko za jednym razem
+    tbody.appendChild(fragment);
+})
+.catch(() => console.error('Błąd pobierania danych.'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,37 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-
+  mode: "production",
   entry: "./src/js/main.js",
-
   output: {
-    filename: "main.js",
+    filename: "js/[name].[contenthash].js", // Hash zapobiega problemom z cache (Efficient cache lifetimes)
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    clean: true, // Czyści folder dist przed każdym buildem
   },
-
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [
+          MiniCssExtractPlugin.loader, // style-loader
+          "css-loader",
+          "sass-loader"
+        ],
       },
     ],
   },
-
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", 
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.[contenthash].css",
+    }),
+  ],
   devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
+    static: "./dist",
     open: true,
-    port: 8080,
   },
 };
